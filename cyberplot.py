0cec97c3440ca4aec0c6fbaa390cba070585ac32
Tomires <me@tomires.eu>
2019-06-24 01:51:01 +0900

    Distinguish between creating and updating dataset

--- cyberplot.py
+++ cyberplot.py
@@ -8,6 +8,19 @@ UPLOAD_PATH = "/api/dataset_upload/"
 
 class cyberplot:
     def new(self, dataFrame, id, name, serverUrl = None):
+        if not name:
+            print("Please specify dataset name.")
+
+        self.__upload(dataFrame, id, name, serverUrl, False)
+
+    def update(self, dataFrame, id, serverUrl = None):
+        self.__upload(dataFrame, id, None, serverUrl, True)
+
+    def __upload(self, dataFrame, id, name, serverUrl, updating):
+        if not id:
+            print("Please specify identifier.")
+            return
+
         dataFrame.to_csv("_cp_dataset.csv", index = False)
 
         usedUrl = serverUrl
@@ -15,7 +28,7 @@ class cyberplot:
             usedUrl = DEFAULT_URL
         usedUrl += ":" + PORT + UPLOAD_PATH
 
-        metadata = {"json": [{"json": {"name": name, "identifier": id, "containsHeader": 1}}]}
+        metadata = {"json": [{"json": {"name": name, "identifier": id, "containsHeader": 1, "updating": updating}}]}
         files = {"file": open("_cp_dataset.csv", "rb")}
         response = requests.post(usedUrl, files = files, data = metadata, verify = False)
 
@@ -28,4 +41,4 @@ class cyberplot:
 
 from pydataset import data
 cp = cyberplot()
-cp.new(data("iris"), id = "424bbe505a1af797062c01972a13baf1", name = "Iris Python")
+cp.new(data("iris"), id = "123b7d7f8de0873d713e1e6c036c20cd", name = "Iris Dataset")
